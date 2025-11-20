84783b8d4e1704e6025035ea50b8de7eb5bb8b65
S <stevenxsx@gmail.com>
2025-11-20 14:27:39 +0100

    Small changes

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ def main():
     network.add_layer(Dense(2, 2, Tanh()))
     print(network.forward(np.array([3, 3])))
 
+    #TODO: implement training and prediction example
+    #network.train(X_train, y_train, epochs=1000, learning_rate=0.01)
+    #prediction = network.predict(X_test)
+
 
 if __name__ == "__main__":
     main()
 
-#test commit 4
-#test commit 5
