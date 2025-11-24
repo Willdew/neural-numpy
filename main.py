86d3d58a9df40a11db0ddb7f0b20295d1a4e0b18
S <stevenxsx@gmail.com>
2025-11-24 13:46:32 +0100

    Added method for downloading, formatting, and loading the CIFAR-10 dataset.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from fileinput import filename
 import numpy as np
+import pickle
 from numpy_nn.network import NeuralNetwork
 from numpy_nn.layer import Dense
 from numpy_nn.activation import Tanh
 from numpy_nn.loss import MSE
 
+import urllib.request
+import tarfile
+import os
 
 def test_xor():
     """Test the network on the XOR problem - a classic non-linear problem"""
@@ -75,8 +80,59 @@ def main():
     np.random.seed(42)
     
     # Run tests
-    test_xor()
-    test_simple_regression()
+    #test_xor()
+    #test_simple_regression()
+    downloadDataset()
+    X_train = []
+    y_train = []
+    
+    for i in range(1, 6):
+        imgs, labels = load_batch(f"./cifar-10-batches-py/data_batch_{i}")
+        X_train.append(imgs)
+        y_train.append(labels)
+    
+    X_train = np.concatenate(X_train, axis=0)
+    y_train = np.concatenate(y_train, axis=0)
+    
+    X_test, y_test = load_batch("./cifar-10-batches-py/test_batch")
+    
+    print(X_train.shape)   # (50000, 32, 32, 3)
+    print(y_train.shape)   # (50000,)
+    
+    print(X_test.shape)    # (10000, 32, 32, 3)
+    print(y_test.shape)    # (10000,)
+
+
+def downloadDataset():
+    url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+    filename = "cifar-10-python.tar.gz"
+
+    # Download file
+    if not os.path.exists(filename):
+        print("Downloading CIFAR-10...")
+        urllib.request.urlretrieve(url, filename)
+
+    # Extract it
+    if not os.path.exists("./cifar-10-batches-py"):
+        print("Extracting...")
+        with tarfile.open(filename, "r:gz") as tar:
+            tar.extractall()
+
+def load_batch(batch_path):
+    with open(batch_path, 'rb') as f:
+        batch = pickle.load(f, encoding='bytes')
+    # Convert keys from bytes to strings
+    batch = {k.decode(): v for k, v in batch.items()}
+    
+    data = batch["data"]               # shape: (10000, 3072)
+    labels = batch["labels"]           # list of 10000 integers
+    filenames = batch["filenames"]     # list of names
+
+    # Reshape to images: (10000, 32, 32, 3)
+    images = data.reshape((10000, 3, 32, 32)).transpose(0, 2, 3, 1)
+    
+    return images, np.array(labels)
+
 
 
 if __name__ == "__main__":
