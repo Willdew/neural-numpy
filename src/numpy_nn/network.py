c750560d091f7627c2b1e131ec6cc10237d57fec
willdew <william.brochner@gmail.com>
2025-11-30 23:41:53 +0100

    updated training function for optimizer

--- src/numpy_nn/network.py
+++ src/numpy_nn/network.py
@@ -1,4 +1,6 @@
 from typing import List
+
+from numpy_nn.optimizer import Optimizer
 from .layer import Layer
 import numpy as np
 import wandb
@@ -24,32 +26,37 @@ class NeuralNetwork:
             output = layer.forward(output)
         return output
 
-    def backprop(self, loss_gradient: np.ndarray, learning_rate: float):
+    def backprop(self, loss_gradient: np.ndarray):
         grad = loss_gradient
         for layer in reversed(self.__layers):
-            grad = layer.backward(grad, learning_rate)
+            grad = layer.backward(grad)
 
-    def train(self, X: np.ndarray, y: np.ndarray, loss_function, epochs: int, learning_rate: float, printProgressRate: int = 100):
+    def train(
+        self,
+        X: np.ndarray,
+        y: np.ndarray,
+        loss_function,
+        epochs: int,
+        optimizer: Optimizer,
+        printProgressRate: int = 100,
+    ):
         for epoch in range(epochs):
             # Forward pass
             predictions = self.forward(X)
 
             # Compute loss and its gradient
             loss = loss_function.forward(predictions, y)
-            loss_gradient = loss_function.backward(predictions, y)
 
-            # Compute accuracy
-            Accuracy = np.mean(np.argmax(predictions, axis=1) == np.argmax(y, axis=1))
+            for layer in self.__layers:
+                optimizer.update(layer)
 
-            # Backward pass
-            self.backprop(loss_gradient, learning_rate)
+            acc = np.mean(np.argmax(predictions, axis=1) == np.argmax(y, axis=1))
 
             if (epoch + 1) % printProgressRate == 0 or epoch == 0:
-                print(f"Epoch {epoch + 1}/{epochs}, Loss: {loss}, Accuracy: {Accuracy}")
+                print(f"Epoch {epoch + 1}/{epochs}, Loss: {loss}, Accuracy: {acc}")
 
             # Log training progress to wandb
-            wandb.log({"loss": float(loss), "acc": float(Accuracy)})
-        
+            wandb.log({"loss": float(loss), "acc": float(acc)})
 
 
 # Builder method for constructing a new network, doesen't really do anything yet
