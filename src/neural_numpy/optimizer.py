ef563b8c7c391ac82ce127f298ce1fc7ebf015a9
S <stevenxsx@gmail.com>
2025-12-01 14:38:18 +0100

    Added ADAM optimizer

--- src/neural_numpy/optimizer.py
+++ src/neural_numpy/optimizer.py
@@ -56,3 +56,44 @@ class SGD(Optimizer):
                 param.data -= self.velocities[p_id]
             else:
                 param.data -= self.learning_rate * param.grad
+
+class ADAM(Optimizer):
+    """
+    Adam optimizer, our favorite optimizer for deep learning :))
+    It adapts the learning rate for each parameter based on the first and second moments of the gradients.
+    """
+
+    def __init__(self, learning_rate: float = 0.001, beta1: float = 0.9, beta2: float = 0.999, epsilon: float = 1e-8):
+        super().__init__(learning_rate)
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.m = {}
+        self.v = {}
+        self.t = 0
+
+    def step(self, layer):
+        self.t += 1
+        for param in layer.get_parameters():
+            if param.grad is None:
+                continue
+
+            p_id = id(param)
+
+            if p_id not in self.m:
+                self.m[p_id] = np.zeros_like(param.data)
+                self.v[p_id] = np.zeros_like(param.data)
+
+            self.m[p_id] = (
+                self.beta1 * self.m[p_id] + (1 - self.beta1) * param.grad
+            )
+            self.v[p_id] = (
+                self.beta2 * self.v[p_id] + (1 - self.beta2) * (param.grad ** 2)
+            )
+
+            m_hat = self.m[p_id] / (1 - self.beta1 ** self.t)
+            v_hat = self.v[p_id] / (1 - self.beta2 ** self.t)
+
+            param.data -= (
+                self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon)
+            )
